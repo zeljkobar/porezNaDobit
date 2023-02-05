e565d3a1a63ff5222c738fe1261b578ea3ed03db
zeljkobar <zeljkobar@gmail.com>
2023-02-05 18:55:03 +0100

    start

---
+++ podaci.js
@@ -0,0 +1,207 @@
+const zavet = {
+  ime: "Zavet d.o.o.",
+  pib: "02793253",
+  adresa: "VUKA KARADZICA BB",
+  pdv: "80/31-02314-8",
+};
+
+const volume = {
+  ime: "Volume d.o.o.",
+  pib: "02807572",
+  adresa: "Rutke bb",
+  pdv: "80/31-02174-0",
+};
+
+const wGroup = {
+  ime: "W Group d.o.o.",
+  pib: "02670739",
+  adresa: "Bjelisi 260",
+  pdv: "80/31-01444-2",
+};
+
+const ajna = {
+  ime: "Ajna d.o.o.",
+  pib: "02984296",
+  adresa: "Stari Bar bb",
+  pdv: "80/31-02906-7",
+};
+
+const nddl = {
+  ime: "NDDL d.o.o.",
+  pib: "02992507",
+  adresa: "Mirosica 2",
+  pdv: "80/31-02947-4",
+};
+
+const mart = {
+  ime: "8 Mart d.o.o.",
+  pib: "02224399",
+  adresa: "Jovana Tomasevica 32/34",
+  pdv: "80/31-00172-3",
+};
+
+const benjoTravel = {
+  ime: "Benjo travel d.o.o.",
+  pib: "02995514",
+  adresa: "Sokolana bb",
+  pdv: "80/31-02956-3",
+};
+
+const adriaticProing = {
+  ime: "Adriatic proing d.o.o.",
+  pib: "02758342",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/31-03772-8",
+};
+
+const figther = {
+  ime: "Fighter d.o.o.",
+  pib: "03046800",
+  adresa: "Susanj bb",
+  pdv: "",
+};
+
+const creativeSolutionsMontenegro = {
+  ime: "Creative Solutions Montenegro d.o.o.",
+  pib: "03057925",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/31-03213-0",
+};
+
+const asg = {
+  ime: "Adriatic Style Group d.o.o.",
+  pib: "02679787",
+  adresa: "Bjelisi bb",
+  pdv: "80/31-1497-3",
+};
+
+const nivl = {
+  ime: "Nivl d.o.o.",
+  pib: "03111997",
+  adresa: "Poslovni centar G9",
+  pdv: "80/31-03508-3",
+};
+
+const butua = {
+  ime: "Butua resort d.o.o.",
+  pib: "02622777",
+  adresa: "Marsala Tita zgrada D5",
+  pdv: "",
+};
+
+const ulcinj = {
+  ime: "Ulcinj resort d.o.o.",
+  pib: "02622521",
+  adresa: "Marsala Tita zgrada D5",
+  pdv: "92/31-00893-1",
+};
+
+const xray = {
+  ime: "Xray d.o.o.",
+  pib: "03284093",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/31-04266-7",
+};
+
+const panKovTrading = {
+  ime: "Pan.kov trading d.o.o.",
+  pib: "03259196",
+  adresa: "Velika Gorana bb",
+  pdv: "80-31-04220-9",
+};
+
+const paKo = {
+  ime: "Pa.ko international trading d.o.o.",
+  pib: "03235939",
+  adresa: "Muftije Karadjuzovica bb",
+  pdv: "80/31-04005-2",
+};
+const cocoLocoFashion = {
+  ime: "Coco Loco fashion d.o.o. ",
+  pib: "03093913",
+  adresa: "Drobnici bb",
+  pdv: "81/31-04899-9",
+};
+
+const aesha = {
+  ime: "Aesha d.o.o.",
+  pib: "03289052",
+  adresa: "Bjelisi bb",
+  pdv: "70/31-02212-0",
+};
+
+const welWar = {
+  ime: "Wel-War d.o.o.",
+  pib: "03103269",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/31-03525-3",
+};
+
+const elida = {
+  ime: "Elida Sahinovic d.o.o.",
+  pib: "02066602",
+  adresa: "Topolica D9",
+  pdv: "",
+};
+
+const jokicZp = {
+  ime: "Jokic zp d.o.o.",
+  pib: "02670194",
+  adresa: "Ilino b.b.",
+  pdv: "80/31-01845-6",
+};
+
+const mdAlfa = {
+  ime: "Md alfa d.o.o.",
+  pib: "02940442",
+  adresa: "Mila Damjanovica 5",
+  pdv: "80/31-02762-5",
+};
+
+const ninaMont = {
+  ime: "Nina mont d.o.o.",
+  pib: "02648938",
+  adresa: "Bulevar Marka Brezanina bb",
+  pdv: "80/31-016232",
+};
+const phoenixPetroleum = {
+  ime: "Phoenix petroleum d.o.o.",
+  pib: "02755343",
+  adresa: "Popovici b.b.",
+  pdv: "80/31-01894-4",
+};
+
+const iiiris = {
+  ime: "II - IRIS DOO",
+  pib: "02704951",
+  adresa: "Boljevici bb",
+  pdv: "80/31-01637-2",
+};
+const firme = [
+  zavet,
+  volume,
+  wGroup,
+  ajna,
+  nddl,
+  mart,
+  benjoTravel,
+  adriaticProing,
+  figther,
+  creativeSolutionsMontenegro,
+  asg,
+  nivl,
+  butua,
+  ulcinj,
+  xray,
+  panKovTrading,
+  paKo,
+  cocoLocoFashion,
+  aesha,
+  welWar,
+  elida,
+  jokicZp,
+  mdAlfa,
+  ninaMont,
+  phoenixPetroleum,
+  iiiris,
+];
