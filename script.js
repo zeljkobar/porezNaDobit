42c91df6e1e9d06c001336f1639551eee26ef542
Zeljko Djuranovic <zeljkobar@gmail.com>
2023-02-06 02:03:58 +0100

    napravljen xml

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+let izabranaFirma;
+let pib;
+let adresa;
+let godina = 2022;
+let Izmijenjena = false;
+let naziv;
+let OvlascenoLicePrezimeIme = 'Zeljko Djuranovic';
+let OvlascenoLicePIB = '1606981220012';
+let KontaktTelefon = '067440040'
+let KontaktEmail = 'zeljkodj@t-com.me'
+const form = document.querySelector("form");
+const inputs = form.querySelectorAll("input");
+const select = document.getElementById("mySelect");
+const vrijednostiPolja = []
+
+
+
+// dodaje sve inpute u niz vrijednostiPolja
+inputs.forEach(function (input) {
+  input.addEventListener("input", function () {
+    vrijednostiPolja[input.name] = parseFloat(input.value);
+  });
+});
+
+
+// funkcija za izvoz xml-a
 function download(filename, text) {
   var element = document.createElement("a");
   element.style.display = "none";
@@ -12,8 +38,8 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
-const select = document.getElementById("mySelect");
 
+// kreira dropdown listu
 for (const item of firme) {
   const option = document.createElement("option");
   option.value = item.ime;
@@ -21,17 +47,7 @@ for (const item of firme) {
   select.appendChild(option);
 }
 
-let izabranaFirma;
-let pib;
-let adresa;
-let godina = 2022;
-let Izmijenjena = false;
-let naziv;
-let OvlascenoLicePrezimeIme = 'Zeljko Djuranovic';
-let OvlascenoLicePIB = '1606981220012';
-let KontaktTelefon = '067440040'
-
-const form = document.querySelector("form");
+// prelazak na enter na novo polje
 form.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
@@ -44,6 +60,7 @@ form.addEventListener("keydown", function (event) {
   }
 });
 
+// selektuje izabranu firmu
 select.addEventListener("change", (event) => {
   const selectedIndex = event.target.selectedIndex;
   izabranaFirma = firme[selectedIndex];
@@ -51,130 +68,135 @@ select.addEventListener("change", (event) => {
   pib = izabranaFirma.pib;
   adresa = izabranaFirma.adresa;
 });
-const x01 = document.getElementById('x01');
-console.log(pib);
-console.log(izabranaFirma);
 
-document.getElementById('download-btn').addEventListener('click', function () {
-  console.log(x01.value);
+
+document.getElementById('proba').addEventListener('click', function () {
+  let rezultat = vrijednostiPolja['x01'] - vrijednostiPolja['x03'] + vrijednostiPolja['x04']
+  console.log(vrijednostiPolja);
+  console.log(vrijednostiPolja['x03'])
+  console.log(typeof (vrijednostiPolja['x03']))
+  console.log(rezultat);
+
 })
-// document.getElementById("download-btn").addEventListener(
-//   "click",
-//   function () {
-//     console.log(select.value);
-//     var text = `<?xml version="1.0" encoding="utf-8"?>
-//     <PortalCitReturn2018 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
-//       <PIB>03356299</PIB>
-//       <Godina>2022</Godina>
-//       <Od>2022-01-01T00:00:00</Od>
-//       <Do>2022-12-31T00:00:00</Do>
-//       <Izmijenjena>false</Izmijenjena>
-//       <Naziv>SAMRAJ DOO BAR</Naziv>
-//       <Rezident>true</Rezident>
-//       <Adresa> , BAR</Adresa>
-//       <OvlascenoLicePIB>1606981220012</OvlascenoLicePIB>
-//       <OvlascenoLicePrezimeIme>Željko Ðuranoviæ</OvlascenoLicePrezimeIme>
-//       <KontaktEmail>zeljkodj@t-com.me</KontaktEmail>
-//       <KontaktTelefon>067440040</KontaktTelefon>
-//       <KonsOdobreno>ne</KonsOdobreno>
-//       <KonsDatumRjesenja xsi:nil="true" />
-//       <Iznos1>0</Iznos1>
-//       <Iznos2>1729</Iznos2>
-//       <Iznos3>0</Iznos3>
-//       <Iznos4>0</Iznos4>
-//       <Iznos5>0</Iznos5>
-//       <Iznos6>0</Iznos6>
-//       <Iznos7>0</Iznos7>
-//       <Iznos8>0</Iznos8>
-//       <Iznos9>0</Iznos9>
-//       <Iznos10>0</Iznos10>
-//       <Iznos11>0</Iznos11>
-//       <Iznos12>0</Iznos12>
-//       <Iznos13>0</Iznos13>
-//       <Iznos14>0</Iznos14>
-//       <Iznos15>0</Iznos15>
-//       <Iznos16>0</Iznos16>
-//       <Iznos17>0</Iznos17>
-//       <Iznos18>0</Iznos18>
-//       <Iznos19>0</Iznos19>
-//       <Iznos20>0</Iznos20>
-//       <Iznos21>0</Iznos21>
-//       <Iznos22>0</Iznos22>
-//       <Iznos23>0</Iznos23>
-//       <Iznos24>0</Iznos24>
-//       <Iznos25>0</Iznos25>
-//       <Iznos26>0</Iznos26>
-//       <Iznos27>0</Iznos27>
-//       <Iznos28>0</Iznos28>
-//       <Iznos29>0</Iznos29>
-//       <Iznos30>0</Iznos30>
-//       <Iznos31>0</Iznos31>
-//       <Iznos32>0</Iznos32>
-//       <Iznos33>0</Iznos33>
-//       <Iznos34>0</Iznos34>
-//       <Iznos35>0</Iznos35>
-//       <Iznos36>0</Iznos36>
-//       <Iznos37>1729</Iznos37>
-//       <Iznos38>0</Iznos38>
-//       <Iznos39>0</Iznos39>
-//       <Iznos40>0</Iznos40>
-//       <Iznos41>0</Iznos41>
-//       <Iznos42>0</Iznos42>
-//       <Iznos43>0</Iznos43>
-//       <Iznos44>0</Iznos44>
-//       <Iznos45>0</Iznos45>
-//       <Iznos46>0</Iznos46>
-//       <Iznos47>0</Iznos47>
-//       <Iznos48>0</Iznos48>
-//       <Iznos49a>0</Iznos49a>
-//       <Iznos49b>0</Iznos49b>
-//       <Iznos49c>0</Iznos49c>
-//       <Iznos50>0</Iznos50>
-//       <Iznos51>0</Iznos51>
-//       <Iznos52>0</Iznos52>
-//       <Iznos53>0</Iznos53>
-//       <Iznos54>0</Iznos54>
-//       <Iznos55>0</Iznos55>
-//       <Iznos56>0</Iznos56>
-//       <Iznos57>0</Iznos57>
-//       <Iznos58>0</Iznos58>
-//       <Iznos59>0</Iznos59>
-//       <Iznos60>0</Iznos60>
-//       <Iznos61>0</Iznos61>
-//       <Iznos62>0</Iznos62>
-//       <IznosPG1_1>0</IznosPG1_1>
-//       <IznosPG1_2>621</IznosPG1_2>
-//       <IznosPG1_21>621</IznosPG1_21>
-//       <IznosPG1_22>0</IznosPG1_22>
-//       <IznosPG1_23>0</IznosPG1_23>
-//       <IznosPG1_24>0</IznosPG1_24>
-//       <IznosPG1_25>0</IznosPG1_25>
-//       <IznosPG1_3>0</IznosPG1_3>
-//       <IznosPG2_1>0</IznosPG2_1>
-//       <IznosPG2_2>0</IznosPG2_2>
-//       <IznosPG2_21>0</IznosPG2_21>
-//       <IznosPG2_22>0</IznosPG2_22>
-//       <IznosPG2_23>0</IznosPG2_23>
-//       <IznosPG2_24>0</IznosPG2_24>
-//       <IznosPG2_25>0</IznosPG2_25>
-//       <IznosPG2_3>0</IznosPG2_3>
-//       <BrojDokumentaOrgId></BrojDokumentaOrgId>
-//       <BrojDokumentaRbr>0</BrojDokumentaRbr>
-//       <Akcija>new</Akcija>
-//       <Message>
-//         <Show>false</Show>
-//         <Message />
-//       </Message>
-//       <Error>
-//         <Show>false</Show>
-//         <Message />
-//       </Error>
-//     </PortalCitReturn2018>
-//     `;
-//     console.log(izabranaFirma.ime);
-//     //   var filename = document.getElementById("filename").value;
-//     var filename = `${izabranaFirma.ime}.xml`;
-//     download(filename, text);
-//   },
-//   false
-// );
+
+// kreira xml
+document.getElementById("download-btn").addEventListener(
+  "click",
+  function () {
+
+    var text = `<?xml version="1.0" encoding="utf-8"?>
+    <PortalCitReturn2018 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+      <PIB>${pib}</PIB>
+      <Godina>${godina}</Godina>
+      <Od>2022-01-01T00:00:00</Od>
+      <Do>2022-12-31T00:00:00</Do>
+      <Izmijenjena>${Izmijenjena}</Izmijenjena>
+      <Naziv>${izabranaFirma.ime}</Naziv>
+      <Rezident>true</Rezident>
+      <Adresa>${izabranaFirma.adresa}</Adresa>
+      <OvlascenoLicePIB>${OvlascenoLicePIB}</OvlascenoLicePIB>
+      <OvlascenoLicePrezimeIme>${OvlascenoLicePrezimeIme}</OvlascenoLicePrezimeIme>
+      <KontaktEmail>${KontaktEmail}</KontaktEmail>
+      <KontaktTelefon>${KontaktTelefon}</KontaktTelefon>
+      <KonsOdobreno>ne</KonsOdobreno>
+      <KonsDatumRjesenja xsi:nil="true" />
+      <Iznos1>${vrijednostiPolja['x01']}</Iznos1>
+      <Iznos2>${vrijednostiPolja['x02']}</Iznos2>
+      <Iznos3>${vrijednostiPolja['x03']}</Iznos3>
+      <Iznos4>${vrijednostiPolja['x04']}</Iznos4>
+      <Iznos5>${vrijednostiPolja['x05']}</Iznos5>
+      <Iznos6>${vrijednostiPolja['x06']}</Iznos6>
+      <Iznos7>${vrijednostiPolja['x07']}</Iznos7>
+      <Iznos8>${vrijednostiPolja['x08']}</Iznos8>
+      <Iznos9>${vrijednostiPolja['x09']}</Iznos9>
+      <Iznos10>${vrijednostiPolja['x10']}</Iznos10>
+      <Iznos11>${vrijednostiPolja['x11']}</Iznos11>
+      <Iznos12>${vrijednostiPolja['x12']}</Iznos12>
+      <Iznos13>${vrijednostiPolja['x13']}</Iznos13>
+      <Iznos14>${vrijednostiPolja['x14']}</Iznos14>
+      <Iznos15>${vrijednostiPolja['x15']}</Iznos15>
+      <Iznos16>${vrijednostiPolja['x16']}</Iznos16>
+      <Iznos17>${vrijednostiPolja['x17']}</Iznos17>
+      <Iznos18>${vrijednostiPolja['x18']}</Iznos18>
+      <Iznos19>${vrijednostiPolja['x19']}</Iznos19>
+      <Iznos20>${vrijednostiPolja['x20']}</Iznos20>
+      <Iznos21>${vrijednostiPolja['x21']}</Iznos21>
+      <Iznos22>${vrijednostiPolja['x22']}</Iznos22>
+      <Iznos23>${vrijednostiPolja['x23']}</Iznos23>
+      <Iznos24>${vrijednostiPolja['x24']}</Iznos24>
+      <Iznos25>${vrijednostiPolja['x25']}</Iznos25>
+      <Iznos26>${vrijednostiPolja['x26']}</Iznos26>
+      <Iznos27>${vrijednostiPolja['x27']}</Iznos27>
+      <Iznos28>${vrijednostiPolja['x28']}</Iznos28>
+      <Iznos29>${vrijednostiPolja['x29']}</Iznos29>
+      <Iznos30>${vrijednostiPolja['x30']}</Iznos30>
+      <Iznos31>${vrijednostiPolja['x31']}</Iznos31>
+      <Iznos32>${vrijednostiPolja['x32']}</Iznos32>
+      <Iznos33>${vrijednostiPolja['x33']}</Iznos33>
+      <Iznos34>${vrijednostiPolja['x34']}</Iznos34>
+      <Iznos35>${vrijednostiPolja['x35']}</Iznos35>
+      <Iznos36>${vrijednostiPolja['x36']}</Iznos36>
+      <Iznos37>${vrijednostiPolja['x37']}</Iznos37>
+      <Iznos38>${vrijednostiPolja['x38']}</Iznos38>
+      <Iznos39>${vrijednostiPolja['x39']}</Iznos39>
+      <Iznos40>${vrijednostiPolja['x40']}</Iznos40>
+      <Iznos41>${vrijednostiPolja['x41']}</Iznos41>
+      <Iznos42>${vrijednostiPolja['x42']}</Iznos42>
+      <Iznos43>${vrijednostiPolja['x43']}</Iznos43>
+      <Iznos44>${vrijednostiPolja['x44']}</Iznos44>
+      <Iznos45>${vrijednostiPolja['x45']}</Iznos45>
+      <Iznos46>${vrijednostiPolja['x46']}</Iznos46>
+      <Iznos47>${vrijednostiPolja['x47']}</Iznos47>
+      <Iznos48>${vrijednostiPolja['x48']}</Iznos48>
+      <Iznos49a>${vrijednostiPolja['x49a']}</Iznos49a>
+      <Iznos49b>${vrijednostiPolja['x49b']}</Iznos49b>
+      <Iznos49c>${vrijednostiPolja['x49c']}</Iznos49c>
+      <Iznos50>${vrijednostiPolja['x50']}</Iznos50>
+      <Iznos51>${vrijednostiPolja['x51']}</Iznos51>
+      <Iznos52>${vrijednostiPolja['x52']}</Iznos52>
+      <Iznos53>${vrijednostiPolja['x53']}</Iznos53>
+      <Iznos54>${vrijednostiPolja['x54']}</Iznos54>
+      <Iznos55>${vrijednostiPolja['x55']}</Iznos55>
+      <Iznos56>${vrijednostiPolja['x56']}</Iznos56>
+      <Iznos57>${vrijednostiPolja['x57']}</Iznos57>
+      <Iznos58>${vrijednostiPolja['x58']}</Iznos58>
+      <Iznos59>${vrijednostiPolja['x59']}</Iznos59>
+      <Iznos60>${vrijednostiPolja['x60']}</Iznos60>
+      <Iznos61>${vrijednostiPolja['x61']}</Iznos61>
+      <Iznos62>${vrijednostiPolja['x62']}</Iznos62>
+      <IznosPG1_1>0</IznosPG1_1>
+      <IznosPG1_2>621</IznosPG1_2>
+      <IznosPG1_21>621</IznosPG1_21>
+      <IznosPG1_22>0</IznosPG1_22>
+      <IznosPG1_23>0</IznosPG1_23>
+      <IznosPG1_24>0</IznosPG1_24>
+      <IznosPG1_25>0</IznosPG1_25>
+      <IznosPG1_3>0</IznosPG1_3>
+      <IznosPG2_1>0</IznosPG2_1>
+      <IznosPG2_2>0</IznosPG2_2>
+      <IznosPG2_21>0</IznosPG2_21>
+      <IznosPG2_22>0</IznosPG2_22>
+      <IznosPG2_23>0</IznosPG2_23>
+      <IznosPG2_24>0</IznosPG2_24>
+      <IznosPG2_25>0</IznosPG2_25>
+      <IznosPG2_3>0</IznosPG2_3>
+      <BrojDokumentaOrgId></BrojDokumentaOrgId>
+      <BrojDokumentaRbr>0</BrojDokumentaRbr>
+      <Akcija>new</Akcija>
+      <Message>
+        <Show>false</Show>
+        <Message />
+      </Message>
+      <Error>
+        <Show>false</Show>
+        <Message />
+      </Error>
+    </PortalCitReturn2018>
+    `;
+    console.log(izabranaFirma.ime);
+    //   var filename = document.getElementById("filename").value;
+    var filename = `${izabranaFirma.ime}.xml`;
+    download(filename, text);
+  },
+  false
+);
