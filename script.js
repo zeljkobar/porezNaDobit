97d48dd0a16ff743c2ac51c876256e90dbd273b7
zeljkobar <zeljkobar@gmail.com>
2023-02-06 14:36:42 +0100

    unijet uslov dobiti

--- script.js
+++ script.js
@@ -1,43 +1,121 @@
 let izabranaFirma, naziv, pib, adresa;
 let godina = 2022;
 let Izmijenjena = false;
-let OvlascenoLicePrezimeIme = 'Zeljko Djuranovic';
-let OvlascenoLicePIB = '1606981220012';
-let KontaktTelefon = '067440040'
-let KontaktEmail = 'zeljkodj@t-com.me'
+let OvlascenoLicePrezimeIme = "Zeljko Djuranovic";
+let OvlascenoLicePIB = "1606981220012";
+let KontaktTelefon = "067440040";
+let KontaktEmail = "zeljkodj@t-com.me";
 const form = document.querySelector("form");
 const inputs = form.querySelectorAll("input");
 const select = document.getElementById("mySelect");
-const vrijednostiPolja = []
-let rezultat, dobit9, dobit12, dobit15, gubitak;
-
+const vrijednostiPolja = [];
+let rezultat, dobit9, dobit12, dobit15, gubitak, dobit;
 
 // dodaje sve inpute u niz vrijednostiPolja
 inputs.forEach(function (input) {
-  vrijednostiPolja[input.name] = 0
+  vrijednostiPolja[input.name] = 0;
   input.addEventListener("input", function () {
     vrijednostiPolja[input.name] = parseFloat(input.value);
     console.log(vrijednostiPolja);
-    preracun(vrijednostiPolja)
+    preracun(vrijednostiPolja);
   });
 });
 
-
 function preracun(vrijednosti) {
-  rezultat = vrijednosti['x01'] - vrijednosti['x03'] + vrijednosti['x04'] + vrijednosti['x05'] - vrijednosti['x06'] + vrijednosti['x07'] + vrijednosti['x08'] + vrijednosti['x09'] + vrijednosti['x10'] + vrijednosti['x11'] + vrijednosti['x12'] + vrijednosti['x13'] + vrijednosti['x14'] + vrijednosti['x15'] + vrijednosti['x16'] + vrijednosti['x17'] + vrijednosti['x18'] + vrijednosti['x19'] + vrijednosti['x20'] + vrijednosti['x21'] + vrijednosti['x22'] + vrijednosti['x23'] + vrijednosti['x24'] + vrijednosti['x27'] + vrijednosti['x28'] + vrijednosti['x29'] + vrijednosti['x32'] - vrijednosti['x33'] - vrijednosti['x34'] - vrijednosti['x35'];
-  if (rezultat < 0) {
-    gubitak = Math.abs(rezultat);
-    dobit9 = 0;
-    dobit12 = 0;
-    dobit15 = 0;
+  if (vrijednosti["x01"]) {
+    rezultat =
+      vrijednosti["x01"] -
+      vrijednosti["x03"] +
+      vrijednosti["x04"] +
+      vrijednosti["x05"] -
+      vrijednosti["x06"] +
+      vrijednosti["x07"] +
+      vrijednosti["x08"] +
+      vrijednosti["x09"] +
+      vrijednosti["x10"] +
+      vrijednosti["x11"] +
+      vrijednosti["x12"] +
+      vrijednosti["x13"] +
+      vrijednosti["x14"] +
+      vrijednosti["x15"] +
+      vrijednosti["x16"] +
+      vrijednosti["x17"] +
+      vrijednosti["x18"] +
+      vrijednosti["x19"] +
+      vrijednosti["x20"] +
+      vrijednosti["x21"] +
+      vrijednosti["x22"] +
+      vrijednosti["x23"] +
+      vrijednosti["x24"] +
+      vrijednosti["x27"] +
+      vrijednosti["x28"] +
+      vrijednosti["x29"] +
+      vrijednosti["x32"] -
+      vrijednosti["x33"] -
+      vrijednosti["x34"] -
+      vrijednosti["x35"];
 
+    console.log("postoji dobit");
+    if (rezultat < 0) {
+      gubitak = Math.abs(rezultat);
+      dobit9 = 0;
+      dobit12 = 0;
+      dobit15 = 0;
+      dobit = 0;
+    } else {
+      dobit = rezultat;
+      gubitak = 0;
+    }
+  } else {
+    rezultat =
+      vrijednosti["x02"] +
+      vrijednosti["x03"] -
+      vrijednosti["x04"] -
+      vrijednosti["x05"] +
+      vrijednosti["x06"] -
+      vrijednosti["x07"] -
+      vrijednosti["x08"] -
+      vrijednosti["x09"] -
+      vrijednosti["x10"] -
+      vrijednosti["x11"] -
+      vrijednosti["x12"] -
+      vrijednosti["x13"] -
+      vrijednosti["x14"] -
+      vrijednosti["x15"] -
+      vrijednosti["x16"] -
+      vrijednosti["x17"] -
+      vrijednosti["x18"] -
+      vrijednosti["x19"] -
+      vrijednosti["x20"] -
+      vrijednosti["x21"] -
+      vrijednosti["x22"] -
+      vrijednosti["x23"] -
+      vrijednosti["x24"] -
+      vrijednosti["x27"] -
+      vrijednosti["x28"] -
+      vrijednosti["x29"] -
+      vrijednosti["x32"] +
+      vrijednosti["x33"] +
+      vrijednosti["x34"] +
+      vrijednosti["x35"];
+    console.log("postoji gubitak");
+
+    if (rezultat < 0) {
+      dobit = Math.abs(rezultat);
+      gubitak = 0;
+    } else {
+      gubitak = rezultat;
+      dobit = 0;
+    }
   }
-  if (rezultat > 0 && rezultat < 100000) {
-    dobit9 = rezultat * 9 / 100;
+
+  if (dobit > 0 && dobit < 100000) {
+    dobit9 = (dobit * 9) / 100;
   }
-  console.log(rezultat);
-  console.log(gubitak);
-  console.log('dobit9 je', dobit9);
+  console.log("rezultatDobit ", rezultat);
+  console.log("gubitak ", gubitak);
+  console.log("dobit ", dobit);
+  console.log("dobit9 je", dobit9);
 }
 
 // funkcija za izvoz xml-a
@@ -55,7 +133,6 @@ function download(filename, text) {
   document.body.removeChild(element);
 }
 
-
 // kreira dropdown listu
 for (const item of firme) {
   const option = document.createElement("option");
@@ -71,7 +148,7 @@ form.addEventListener("keydown", function (event) {
     const fields = form.elements;
     let currentField = event.target;
     let index = Array.prototype.indexOf.call(fields, currentField);
-    if (index !== -1 && (index + 1) < fields.length) {
+    if (index !== -1 && index + 1 < fields.length) {
       fields[index + 1].focus();
     }
   }
@@ -86,21 +163,19 @@ select.addEventListener("change", (event) => {
   adresa = izabranaFirma.adresa;
 });
 
-
-document.getElementById('proba').addEventListener('click', function () {
-  let rezultat = vrijednostiPolja['x01'] - vrijednostiPolja['x03'] + vrijednostiPolja['x04']
+document.getElementById("proba").addEventListener("click", function () {
+  let rezultatDobit =
+    vrijednostiPolja["x01"] - vrijednostiPolja["x03"] + vrijednostiPolja["x04"];
   console.log(vrijednostiPolja);
-  console.log(vrijednostiPolja['x03'])
-  console.log(typeof (vrijednostiPolja['x03']))
-  console.log(rezultat);
-
-})
+  console.log(vrijednostiPolja["x03"]);
+  console.log(typeof vrijednostiPolja["x03"]);
+  console.log(rezultatDobit);
+});
 
 // kreira xml
 document.getElementById("download-btn").addEventListener(
   "click",
   function () {
-
     var text = `<?xml version="1.0" encoding="utf-8"?>
     <PortalCitReturn2018 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
       <PIB>${pib}</PIB>
@@ -117,70 +192,70 @@ document.getElementById("download-btn").addEventListener(
       <KontaktTelefon>${KontaktTelefon}</KontaktTelefon>
       <KonsOdobreno>ne</KonsOdobreno>
       <KonsDatumRjesenja xsi:nil="true" />
-      <Iznos1>${vrijednostiPolja['x01']}</Iznos1>
-      <Iznos2>${vrijednostiPolja['x02']}</Iznos2>
-      <Iznos3>${vrijednostiPolja['x03']}</Iznos3>
-      <Iznos4>${vrijednostiPolja['x04']}</Iznos4>
-      <Iznos5>${vrijednostiPolja['x05']}</Iznos5>
-      <Iznos6>${vrijednostiPolja['x06']}</Iznos6>
-      <Iznos7>${vrijednostiPolja['x07']}</Iznos7>
-      <Iznos8>${vrijednostiPolja['x08']}</Iznos8>
-      <Iznos9>${vrijednostiPolja['x09']}</Iznos9>
-      <Iznos10>${vrijednostiPolja['x10']}</Iznos10>
-      <Iznos11>${vrijednostiPolja['x11']}</Iznos11>
-      <Iznos12>${vrijednostiPolja['x12']}</Iznos12>
-      <Iznos13>${vrijednostiPolja['x13']}</Iznos13>
-      <Iznos14>${vrijednostiPolja['x14']}</Iznos14>
-      <Iznos15>${vrijednostiPolja['x15']}</Iznos15>
-      <Iznos16>${vrijednostiPolja['x16']}</Iznos16>
-      <Iznos17>${vrijednostiPolja['x17']}</Iznos17>
-      <Iznos18>${vrijednostiPolja['x18']}</Iznos18>
-      <Iznos19>${vrijednostiPolja['x19']}</Iznos19>
-      <Iznos20>${vrijednostiPolja['x20']}</Iznos20>
-      <Iznos21>${vrijednostiPolja['x21']}</Iznos21>
-      <Iznos22>${vrijednostiPolja['x22']}</Iznos22>
-      <Iznos23>${vrijednostiPolja['x23']}</Iznos23>
-      <Iznos24>${vrijednostiPolja['x24']}</Iznos24>
-      <Iznos25>${vrijednostiPolja['x25']}</Iznos25>
-      <Iznos26>${vrijednostiPolja['x26']}</Iznos26>
-      <Iznos27>${vrijednostiPolja['x27']}</Iznos27>
-      <Iznos28>${vrijednostiPolja['x28']}</Iznos28>
-      <Iznos29>${vrijednostiPolja['x29']}</Iznos29>
-      <Iznos30>${vrijednostiPolja['x30']}</Iznos30>
-      <Iznos31>${vrijednostiPolja['x31']}</Iznos31>
-      <Iznos32>${vrijednostiPolja['x32']}</Iznos32>
-      <Iznos33>${vrijednostiPolja['x33']}</Iznos33>
-      <Iznos34>${vrijednostiPolja['x34']}</Iznos34>
-      <Iznos35>${vrijednostiPolja['x35']}</Iznos35>
-      <Iznos36>${vrijednostiPolja['x36']}</Iznos36>
-      <Iznos37>${vrijednostiPolja['x37']}</Iznos37>
-      <Iznos38>${vrijednostiPolja['x38']}</Iznos38>
-      <Iznos39>${vrijednostiPolja['x39']}</Iznos39>
-      <Iznos40>${vrijednostiPolja['x40']}</Iznos40>
-      <Iznos41>${vrijednostiPolja['x41']}</Iznos41>
-      <Iznos42>${vrijednostiPolja['x42']}</Iznos42>
-      <Iznos43>${vrijednostiPolja['x43']}</Iznos43>
-      <Iznos44>${vrijednostiPolja['x44']}</Iznos44>
-      <Iznos45>${vrijednostiPolja['x45']}</Iznos45>
-      <Iznos46>${vrijednostiPolja['x46']}</Iznos46>
-      <Iznos47>${vrijednostiPolja['x47']}</Iznos47>
-      <Iznos48>${vrijednostiPolja['x48']}</Iznos48>
-      <Iznos49a>${vrijednostiPolja['x49a']}</Iznos49a>
-      <Iznos49b>${vrijednostiPolja['x49b']}</Iznos49b>
-      <Iznos49c>${vrijednostiPolja['x49c']}</Iznos49c>
-      <Iznos50>${vrijednostiPolja['x50']}</Iznos50>
-      <Iznos51>${vrijednostiPolja['x51']}</Iznos51>
-      <Iznos52>${vrijednostiPolja['x52']}</Iznos52>
-      <Iznos53>${vrijednostiPolja['x53']}</Iznos53>
-      <Iznos54>${vrijednostiPolja['x54']}</Iznos54>
-      <Iznos55>${vrijednostiPolja['x55']}</Iznos55>
-      <Iznos56>${vrijednostiPolja['x56']}</Iznos56>
-      <Iznos57>${vrijednostiPolja['x57']}</Iznos57>
-      <Iznos58>${vrijednostiPolja['x58']}</Iznos58>
-      <Iznos59>${vrijednostiPolja['x59']}</Iznos59>
-      <Iznos60>${vrijednostiPolja['x60']}</Iznos60>
-      <Iznos61>${vrijednostiPolja['x61']}</Iznos61>
-      <Iznos62>${vrijednostiPolja['x62']}</Iznos62>
+      <Iznos1>${vrijednostiPolja["x01"]}</Iznos1>
+      <Iznos2>${vrijednostiPolja["x02"]}</Iznos2>
+      <Iznos3>${vrijednostiPolja["x03"]}</Iznos3>
+      <Iznos4>${vrijednostiPolja["x04"]}</Iznos4>
+      <Iznos5>${vrijednostiPolja["x05"]}</Iznos5>
+      <Iznos6>${vrijednostiPolja["x06"]}</Iznos6>
+      <Iznos7>${vrijednostiPolja["x07"]}</Iznos7>
+      <Iznos8>${vrijednostiPolja["x08"]}</Iznos8>
+      <Iznos9>${vrijednostiPolja["x09"]}</Iznos9>
+      <Iznos10>${vrijednostiPolja["x10"]}</Iznos10>
+      <Iznos11>${vrijednostiPolja["x11"]}</Iznos11>
+      <Iznos12>${vrijednostiPolja["x12"]}</Iznos12>
+      <Iznos13>${vrijednostiPolja["x13"]}</Iznos13>
+      <Iznos14>${vrijednostiPolja["x14"]}</Iznos14>
+      <Iznos15>${vrijednostiPolja["x15"]}</Iznos15>
+      <Iznos16>${vrijednostiPolja["x16"]}</Iznos16>
+      <Iznos17>${vrijednostiPolja["x17"]}</Iznos17>
+      <Iznos18>${vrijednostiPolja["x18"]}</Iznos18>
+      <Iznos19>${vrijednostiPolja["x19"]}</Iznos19>
+      <Iznos20>${vrijednostiPolja["x20"]}</Iznos20>
+      <Iznos21>${vrijednostiPolja["x21"]}</Iznos21>
+      <Iznos22>${vrijednostiPolja["x22"]}</Iznos22>
+      <Iznos23>${vrijednostiPolja["x23"]}</Iznos23>
+      <Iznos24>${vrijednostiPolja["x24"]}</Iznos24>
+      <Iznos25>${vrijednostiPolja["x25"]}</Iznos25>
+      <Iznos26>${vrijednostiPolja["x26"]}</Iznos26>
+      <Iznos27>${vrijednostiPolja["x27"]}</Iznos27>
+      <Iznos28>${vrijednostiPolja["x28"]}</Iznos28>
+      <Iznos29>${vrijednostiPolja["x29"]}</Iznos29>
+      <Iznos30>${vrijednostiPolja["x30"]}</Iznos30>
+      <Iznos31>${vrijednostiPolja["x31"]}</Iznos31>
+      <Iznos32>${vrijednostiPolja["x32"]}</Iznos32>
+      <Iznos33>${vrijednostiPolja["x33"]}</Iznos33>
+      <Iznos34>${vrijednostiPolja["x34"]}</Iznos34>
+      <Iznos35>${vrijednostiPolja["x35"]}</Iznos35>
+      <Iznos36>${dobit}</Iznos36>
+      <Iznos37>${gubitak}</Iznos37>
+      <Iznos38>${vrijednostiPolja["x38"]}</Iznos38>
+      <Iznos39>${vrijednostiPolja["x39"]}</Iznos39>
+      <Iznos40>${vrijednostiPolja["x40"]}</Iznos40>
+      <Iznos41>${vrijednostiPolja["x41"]}</Iznos41>
+      <Iznos42>${vrijednostiPolja["x42"]}</Iznos42>
+      <Iznos43>${vrijednostiPolja["x43"]}</Iznos43>
+      <Iznos44>${vrijednostiPolja["x44"]}</Iznos44>
+      <Iznos45>${vrijednostiPolja["x45"]}</Iznos45>
+      <Iznos46>${vrijednostiPolja["x46"]}</Iznos46>
+      <Iznos47>${vrijednostiPolja["x47"]}</Iznos47>
+      <Iznos48>${vrijednostiPolja["x48"]}</Iznos48>
+      <Iznos49a>${vrijednostiPolja["x49a"]}</Iznos49a>
+      <Iznos49b>${vrijednostiPolja["x49b"]}</Iznos49b>
+      <Iznos49c>${vrijednostiPolja["x49c"]}</Iznos49c>
+      <Iznos50>${vrijednostiPolja["x50"]}</Iznos50>
+      <Iznos51>${vrijednostiPolja["x51"]}</Iznos51>
+      <Iznos52>${vrijednostiPolja["x52"]}</Iznos52>
+      <Iznos53>${vrijednostiPolja["x53"]}</Iznos53>
+      <Iznos54>${vrijednostiPolja["x54"]}</Iznos54>
+      <Iznos55>${vrijednostiPolja["x55"]}</Iznos55>
+      <Iznos56>${vrijednostiPolja["x56"]}</Iznos56>
+      <Iznos57>${vrijednostiPolja["x57"]}</Iznos57>
+      <Iznos58>${vrijednostiPolja["x58"]}</Iznos58>
+      <Iznos59>${vrijednostiPolja["x59"]}</Iznos59>
+      <Iznos60>${vrijednostiPolja["x60"]}</Iznos60>
+      <Iznos61>${vrijednostiPolja["x61"]}</Iznos61>
+      <Iznos62>${vrijednostiPolja["x62"]}</Iznos62>
       <IznosPG1_1>0</IznosPG1_1>
       <IznosPG1_2>0</IznosPG1_2>
       <IznosPG1_21>0</IznosPG1_21>
