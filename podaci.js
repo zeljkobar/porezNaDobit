989aa00d16c91ca7179b3b92de6b27a0d6bd6d69
zeljkobar <zeljkobar@gmail.com>
2023-03-30 17:58:48 +0200

    podaci

--- podaci.js
+++ podaci.js
@@ -325,15 +325,580 @@ const mrJack = {
   pdv: "",
 };
 
+const digitella = {
+  ime: "Digitella d.o.o.",
+  pib: "03337081",
+  adresa: "Utjeha BB",
+  pdv: "",
+};
+
+const hitPlus = {
+  ime: "Hit Plus d.o.o.",
+  pib: "02663481",
+  adresa: "Susanj bb",
+  pdv: "80/31-01399-3",
+};
+
+const otalaki = {
+  ime: "Ota-Laki d.o.o.",
+  pib: "03100863",
+  adresa: "Vladimira rolovica 18",
+  pdv: "80/31-03570-9",
+};
+
+const lgroup = {
+  ime: "L group d.o.o.",
+  pib: "03031063",
+  adresa: "Susanj bb",
+  pdv: "80/31-03108-8",
+};
+const kamine = {
+  ime: "Kamine d.o.o.",
+  pib: "03331288",
+  adresa: "Dubrava 113 A",
+  pdv: "80/31-04441-4",
+};
+
+const inovator = {
+  ime: "Inovator d.o.o.",
+  pib: "03245713",
+  adresa: "Podstustas bb",
+  pdv: "",
+};
+
+const iceman = {
+  ime: "Iceman CG d.o.o.",
+  pib: "03452581",
+  adresa: "Bulevar Revolucije 15",
+  pdv: "",
+};
+const impuls = {
+  ime: "Impuls MB d.o.o.",
+  pib: "02700883",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/13-01593-7",
+};
+const metalplast = {
+  ime: "Metalplast Co d.o.o.",
+  pib: "02820404",
+  adresa: "Haj Nehaj bb",
+  pdv: "80/31-02213-5",
+};
+
+const muriz = {
+  ime: "Muriz d.o.o.",
+  pib: "02068974",
+  adresa: "M. Boskovica h 12/47",
+  pdv: "80/31-00298-3",
+};
+
+const transcargo = {
+  ime: "Trans cargo d.o.o.",
+  pib: "02961377",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/31-02846-8",
+};
+
+const rd1international = {
+  ime: "RD1 international d.o.o.",
+  pib: "03188159",
+  adresa: "Jovana Tomasevica G9",
+  pdv: "80/31-03840-6",
+};
+const kuniversal = {
+  ime: "K Universal d.o.o.",
+  pib: "02624796",
+  adresa: "Rista Lekica",
+  pdv: "80/31-01206-7",
+};
+const coccopazzo = {
+  ime: "Coccopazzo d.o.o.",
+  pib: "02975513",
+  adresa: "Rista Lekica c7",
+  pdv: "80/31-02881-8",
+};
+
+const teodon = {
+  ime: "Teodon md d.o.o.",
+  pib: "02889447",
+  adresa: "Vuka Karadzica 1-2",
+  pdv: "",
+};
+
+const corposano = {
+  ime: "Corpo Sano d.o.o.",
+  pib: "03158128",
+  adresa: "Rista Lekica C1",
+  pdv: "",
+};
+const behar = {
+  ime: "Behar Trade d.o.o.",
+  pib: "03291812",
+  adresa: "Podgrad bb",
+  pdv: "80/31-04280-2",
+};
+const tafos = {
+  ime: "Tafos d.o.o.",
+  pib: "03152707",
+  adresa: "Rista Lekica D11",
+  pdv: "",
+};
+const yomil = {
+  ime: "Yomil d.o.o.",
+  pib: "02335883",
+  adresa: "Trzni centar bb Bar",
+  pdv: "",
+};
+
+const amstrad = {
+  ime: "Amstr@d",
+  pib: "02458900",
+  adresa: "Mila Boskovica 8",
+  pdv: "",
+};
+const efidis = {
+  ime: "Efidis",
+  pib: "02994348",
+  adresa: "IV crnogorska udarna brigada bb",
+  pdv: "",
+};
+
+const bilansm = {
+  ime: "Bilans M d.o.o.",
+  pib: "02965852",
+  adresa: "Jovana Stojanovica 6 / 26",
+  pdv: "",
+};
+
+const viphomes = {
+  ime: "VIP Homes d.o.o.",
+  pib: "02813599",
+  adresa: "Polje bb",
+  pdv: "",
+};
+
+const slijepi = {
+  ime: "NU organizacija slijepih za Bar i Ulcinj",
+  pib: "02040794",
+  adresa: "Rista Lekica D 10",
+  pdv: "",
+};
+const kodPetrovica = {
+  ime: "Kod Petrovica d.o.o.",
+  pib: "02087022",
+  adresa: "Jovana Jovanovica Zmaja 8",
+  pdv: "",
+};
+const npCompany = {
+  ime: "NP Company d.o.o.",
+  pib: "02414058",
+  adresa: "Jovana Jovanovica Zmaja 8",
+  pdv: "",
+};
+
+const mwelnes = {
+  ime: "M wellness trade d.o.o.",
+  pib: "03090655",
+  adresa: "Jovana Tomasevica 18",
+  pdv: "",
+};
+
+const ladorica = {
+  ime: "La Dorica 1 d.o.o.",
+  pib: "02639360",
+  adresa: "Polje bb",
+  pdv: "",
+};
+
+const civili = {
+  ime: "NU organizacija civilnih invalida rata za Bar i Ulcinj",
+  pib: "02084619",
+  adresa: "Rista lekica D 10",
+  pdv: "",
+};
+
+const osi = {
+  ime: "Sportska organizacija / sportsko rekreativno drustvo OSI Bar",
+  pib: "11069576",
+  adresa: "Rista Lekica D-10",
+  pdv: "",
+};
+const bwa = {
+  ime: "BWA d.o.o.",
+  pib: "02423952",
+  adresa: "Blaza Jovanovica 1, Tivat",
+  pdv: "",
+};
+
+const bbsusanj = {
+  ime: "BB Šušanj d.o.o.",
+  pib: "03362400",
+  adresa: "Bjelisi bb",
+  pdv: "80/31-04520-8",
+};
+const amelb = {
+  ime: "Amelb d.o.o.",
+  pib: "03413110",
+  adresa: "Dubrava 9",
+  pdv: "",
+};
+const parus = {
+  ime: "Parus d.o.o.",
+  pib: "02438712",
+  adresa: "Marsala Tita BB",
+  pdv: "80/31-01021-0",
+};
+
+const v3trans = {
+  ime: "V3 Trans d.o.o.",
+  pib: "03441610",
+  adresa: "Kraljice Jelene Balsic 91",
+  pdv: "80/31-04697-2",
+};
+
+const bobantours = {
+  ime: "Boban Tours d.o.o.",
+  pib: "02781905",
+  adresa: "Utjeha bb",
+  pdv: "",
+};
+
+const lukahani = {
+  ime: "Luka & Hany d.o.o.",
+  pib: "03254143",
+  adresa: "Rista Lekica D-9",
+  pdv: "",
+};
+
+const superfenix = {
+  ime: "Super Fenix d.o.o.",
+  pib: "02694131",
+  adresa: "Virpazar BB",
+  pdv: "80/31-01559-7",
+};
+
+const beton = {
+  ime: "Beton Transport Grupa d.o.o.",
+  pib: "02347334",
+  adresa: "Mila Boskovica h12/47",
+  pdv: "",
+};
+
+const sekularac = {
+  ime: "Sekularac Company d.o.o.",
+  pib: "02790858",
+  adresa: "Ilino BB",
+  pdv: "",
+};
+
+const konoba = {
+  ime: "Konoba Vinoteka Virpazar d.o.o.",
+  pib: "03185028",
+  adresa: "Virpazar bb",
+  pdv: "80/31-03827-9",
+};
+
+const ripagent = {
+  ime: "R & P agent d.o.o.",
+  pib: "02758512",
+  adresa: "Marsala Tita 25",
+  pdv: "80/31-02206-2",
+};
+
+const antivari = {
+  ime: "Antivari Star d.o.o.",
+  pib: "03024687",
+  adresa: "Jovana Tomasevica 41",
+  pdv: "80/31-03095-2",
+};
+
+const mbcompany = {
+  ime: "Mb Company d.o.o.",
+  pib: "03097048",
+  adresa: "Vojvode Masa Djurovica",
+  pdv: "",
+};
+
+const malik = {
+  ime: "Malik Bar d.o.o.",
+  pib: "03255379",
+  adresa: "Rusko naselje bb",
+  pdv: "",
+};
+
+const pantelija = {
+  ime: "Pantelija d.o.o.",
+  pib: "03316378",
+  adresa: "Suvi Potok bb",
+  pdv: "80/31-04356-6",
+};
+
+const luksic = {
+  ime: "3 L Company d.o.o.",
+  pib: "03188175",
+  adresa: "Obala Iva Novakovica bb",
+  pdv: "80/31-03839-2",
+};
+
+const winnie = {
+  ime: "Winnie the pooh d.o.o.",
+  pib: "03076784",
+  adresa: "Vladimira Rolovica f-2",
+  pdv: "",
+};
+
+const kormoran = {
+  ime: "Kormoran Virpazar d.o.o.",
+  pib: "03189309",
+  adresa: "Virpazar bb",
+  pdv: "",
+};
+const feiras = {
+  ime: "Feritas d.o.o.",
+  pib: "03098826",
+  adresa: "Carsijska bb",
+  pdv: "61/31-00206-6",
+};
+
+const cnscomapny = {
+  ime: "CNS Company d.o.o.",
+  pib: "03337979",
+  adresa: "Jovana Stojanovica s 43",
+  pdv: "80/31-04524-0",
+};
+const ndmontenegro = {
+  ime: "ND Montenegro d.o.o.",
+  pib: "03280055",
+  adresa: "Bjelila BB",
+  pdv: "",
+};
+const mtmplus = {
+  ime: "MTM Plus d.o.o.",
+  pib: "03089339",
+  adresa: "Tomba bb",
+  pdv: "80/31-03382-8",
+};
+
+const baltez = {
+  ime: "Baltez d.o.o.",
+  pib: "02061341",
+  adresa: "Jovana Stojanovica 8/4",
+  pdv: "80/31-00367-8",
+};
+const capris = {
+  ime: "Capris d.o.o.",
+  pib: "02718375",
+  adresa: "J.J.Zmaja bb",
+  pdv: "80/31-01716-6",
+};
+
+const ssgradnja = {
+  ime: "S&S gradnja d.o.o.",
+  pib: "03397521",
+  adresa: "Bjelisi bb",
+  pdv: "80/31-04598-4",
+};
+
+const babloni = {
+  ime: "Babloniuk d.o.o.",
+  pib: "03304736",
+  adresa: "Bjelisi bb",
+  pdv: "",
+};
+
+const b612 = {
+  ime: "B-612 d.o.o.",
+  pib: "03270548",
+  adresa: "Bjelisi bb",
+};
+
+const lotours = {
+  ime: "Lotours d.o.o. ",
+  pib: "02704943",
+  adresa: "Virpazar bb",
+  pdv: "80/31-01831-6",
+};
+
+const tehnoshop = {
+  ime: "Tehno-shop Bar d.o.o.",
+  pib: "03181227",
+  adresa: "Mirosica II",
+  pdv: "",
+};
+const amsterplus = {
+  ime: "Ansterm plus d.o.o.",
+  pib: "03231577",
+  adresa: "Dobra Voda bb",
+  pdv: "80/31-03986-8",
+};
+const inbar = {
+  ime: "In Bar d.o.o.",
+  pib: "02964279",
+  adresa: "Marsala Tita D5",
+  pdv: "80/31-02842-7",
+};
+const vinarija = {
+  ime: "Vinarija Klisic d.o.o.",
+  pib: "02814145",
+  adresa: "Limljani bb",
+};
+
+const trek = {
+  ime: "Trek Montenegro d.o.o.",
+  pib: "02840685",
+  adresa: "Jovana Tomasevica g9",
+  pdv: "80/31-02400-6",
+};
+const ekogroup = {
+  ime: "Eko Group d.o.o.",
+  pib: "02811472",
+  adresa: "Zaljevo, Gorak bb",
+  pdv: "80/31-02173-2",
+};
+
+const proplast = {
+  ime: "Pro-Plast d.o.o.",
+  pib: "03150925",
+  adresa: "Ruske Barake bb",
+  pdv: "80/31-03710-8",
+};
+
+const termogas = {
+  ime: "Termogas invest d.o.o.",
+  pib: "02785781",
+  adresa: "Njegoseva 6",
+  pdv: "80/31-02075-2",
+};
+
+const vpbarba = {
+  ime: "VP - Barba d.o.o.",
+  pib: "03050858",
+  adresa: "Mirosica II",
+  pdv: "80/31-03236-8",
+};
+
+const neimar = {
+  ime: "Neimar investments construciton",
+  pib: "03317307",
+  adresa: "Susanj bb",
+  pdv: "80/31-04394-9",
+};
+
+const lejla = {
+  ime: "Lejla d.o.o.",
+  pib: "02433583",
+  adresa: "Dobra Voda bb",
+  pdv: "80/31-01116-8",
+};
+
+const onix = {
+  ime: "Onix d.o.o.",
+  pib: "02426129",
+  adresa: "Mirosica II",
+  pdv: "80/31-00980-5",
+};
+
+const fadisco = {
+  ime: "Fadis  Co d.o.o.",
+  pib: "02913569",
+  adresa: "B. Calovica c-10",
+  pdv: "",
+};
+const megtrade = {
+  ime: "Megtrade d.o.o.",
+  pib: "02752450",
+  adresa: "Poslovni Centar G9",
+  pdv: "",
+};
+const srdjokiki = {
+  ime: "Srdjo i Kiki d.o.o.",
+  pib: "03454037",
+  adresa: "Haj Nehaj bb",
+  pdv: "",
+};
+const dvorac = {
+  ime: "Le Petit Chateau d.o.o.",
+  pib: "02876230",
+  Adresa: "Obala Kralja Nikole bb",
+  pdv: "80/31-02479-0",
+};
+const f1international = {
+  ime: "F1 International d.o.o.",
+  pib: "02977796",
+  adresa: "Polje bb",
+  pdv: "80/31-02882-6",
+};
+
+const thgroup = {
+  ime: "TH Group d.o.o.",
+  pib: "03365077",
+  adresa: "Donji Stoj bb",
+  pdv: "",
+};
 const firme = [
   summasummarum,
   opticonnect,
+  transcargo,
+  lejla,
+  luksic,
+  dvorac,
+  f1international,
+  thgroup,
+  srdjokiki,
+  trek,
+  fadisco,
+  neimar,
+  onix,
+  vpbarba,
+  proplast,
+  ekogroup,
+  amelb,
+  megtrade,
+  tehnoshop,
+  ndmontenegro,
+  baltez,
+  inbar,
+  mtmplus,
+  kormoran,
+  cnscomapny,
+  winnie,
+  feiras,
+  antivari,
+  ripagent,
+  pantelija,
+  malik,
+  mbcompany,
+  bobantours,
+  konoba,
+  v3trans,
+  beton,
+  sekularac,
+  superfenix,
+  lukahani,
+  coccopazzo,
+  bbsusanj,
+  parus,
+  behar,
+  kuniversal,
+  hitPlus,
+  metalplast,
+  impuls,
+  muriz,
+  iceman,
+  otalaki,
+  lgroup,
+  kamine,
+  inovator,
+  digitella,
+  rd1international,
   cengic,
   mrJack,
+  teodon,
   stefanoM,
   polarisMaritime,
   knaufSerice,
   bartolomeo,
+  corposano,
   albyInternational,
   samraj,
   fancybar,
@@ -341,6 +906,7 @@ const firme = [
   birozalegalizaciju,
   skerovicgradnja,
   petshopfenix,
+  tafos,
   beautybyroxy,
   oldhousepajovic,
   aginzenjering,
@@ -373,4 +939,25 @@ const firme = [
   phoenixPetroleum,
   iiiris,
   eurowin,
+  yomil,
+  amstrad,
+  efidis,
+  bilansm,
+  viphomes,
+  slijepi,
+  kodPetrovica,
+  npCompany,
+  mwelnes,
+  ladorica,
+  civili,
+  osi,
+  bwa,
+  capris,
+  ssgradnja,
+  babloni,
+  b612,
+  lotours,
+  amsterplus,
+  vinarija,
+  termogas,
 ];
